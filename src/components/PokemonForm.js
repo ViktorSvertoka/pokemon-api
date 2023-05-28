887b152a7dc9292dc611dc302902b80818e38201
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-28 17:36:47 +0300

    Docs(JS) Added comments

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -9,31 +9,41 @@ export default class PokemonForm extends Component {
     pokemonName: '',
   };
 
+  // Обработчик изменения значения в поле ввода
   handleNameChange = event => {
+    // Обновление состояния с введенным именем покемона в нижнем регистре
     this.setState({ pokemonName: event.currentTarget.value.toLowerCase() });
   };
 
   handleSubmit = event => {
     event.preventDefault();
 
+    // Проверка наличия значения в поле ввода
     if (this.state.pokemonName.trim() === '') {
+      // Вывод сообщения об ошибке с помощью toast из react-toastify
       toast.error('Введите имя покемона.');
       return;
     }
 
+    // Вызов onSubmit из пропсов с передачей введенного имени покемона
     this.props.onSubmit(this.state.pokemonName);
+
+    // Сброс значения поля ввода в состоянии
     this.setState({ pokemonName: '' });
   };
 
   render() {
     return (
       <form onSubmit={this.handleSubmit} style={styles.form}>
+        {/* Поле ввода имени покемона */}
         <input
           type="text"
           name="pokemonName"
           value={this.state.pokemonName}
           onChange={this.handleNameChange}
         />
+
+        {/* Кнопка поиска */}
         <button type="submit">
           <ImSearch style={{ marginRight: 8 }} />
           Найти
--- src/services/pokemon-api.js
+++ src/services/pokemon-api.js
@@ -1,15 +1,19 @@
+// Функция для получения данных о покемоне из API
 function fetchPokemon(name) {
   return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then(response => {
     if (response.ok) {
       return response.json();
     }
 
+    // Если ответ не успешный, создается и возвращается отклоненный промис с ошибкой
     return Promise.reject(new Error(`Нет покемона с именем ${name}`));
   });
 }
 
+// Создание объекта `api` с функцией `fetchPokemon` в качестве свойства
 const api = {
   fetchPokemon,
 };
 
+// Экспорт объекта `api` в качестве значения по умолчанию для модуля
 export default api;
